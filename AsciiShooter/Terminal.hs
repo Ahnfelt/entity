10e44f595dc0a969fec6e74b968bd47eadbd6221
Michael Werk <mfwerk@gmail.com>
2011-04-12 12:38:18 +0200

    more tank

--- AsciiShooter/Terminal.hs
+++ AsciiShooter/Terminal.hs
@@ -5,35 +5,49 @@ import Control.Concurrent.STM
 import Control.Concurrent
 import Control.Monad.IO.Class
 import Control.Monad
-import Data.Text hiding (map)
+import Data.Text hiding (map, take, reverse)
 import Data.Array.Diff
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Debug.Trace
 
-data Color = Red
-
+data Color = Red deriving (Eq, Ord, Show)
 data Key = KeyUp | KeyDown | KeyLeft | KeyRight | KeyFire deriving (Eq, Ord, Show)
-
 type Picture = DiffArray (Int, Int) (Char, Color)
+type Position = (Int, Int)
+data Rotaion = North | South | East | West
+type Tank = (Position, Rotaion)
 
 main = runCurses $ do
     window <- defaultWindow
     (rows, columns) <- screenSize
-    let picture = listArray ((0, 0), (fromIntegral columns - 1, fromIntegral rows - 1)) (repeat (' ', Red))
+    let picture = background (columns - 1) (rows - 2)
     pictureVar <- liftIO (newTVarIO picture)
+    tankVar <- liftIO (newTVarIO ((10, 10), North))
     keyChan <- liftIO newTChanIO
     liftIO $ forkIO (controller pictureVar keyChan)
     drawListenLoop window pictureVar keyChan
 
+background :: Int -> Int -> DiffArray (Int, Int) (Char, Color)     
+background width height = 
+    listArray ((0, 0), (fromIntegral width - 1, fromIntegral height - 2)) (repeat (' ', Red))
+
+drawTank :: Picture -> Tank -> Picture
+drawTank picture tanks =
+    let sprite' = translateSprite tankLocation sprite
+    picture <- readTVar pictureVar
+    writeTVar pictureVar (picture // sprite')
+    
+
 draw :: Window -> (Color -> ColorID) -> Picture -> Curses ()
 draw window colors picture = do
+    (rows, columns) <- screenSize
     updateWindow window $ do
         forM_ (assocs picture) $ \((x, y), (character, color)) -> do
             moveCursor (fromIntegral y) (fromIntegral x)
             --trace (show (x, y)) $ do
-            --setColor (colors color)
-            drawText (pack [character])
+            setColor (colors color)
+            drawText (pack ([character]))
     render
 
 
@@ -69,29 +83,56 @@ drawListenLoop window pictureVar keyChan = do
             loop window colors
 
 
-controller :: TVar Picture -> TChan Key -> IO ()
-controller pictureVar keyChan = loop (10, 10)
+controller :: TVar Tank -> TChan Key -> IO ()
+controller tankVar keyChan = loop 
     where
-        loop (x, y) = do
-            tankLocation <- atomically $ do
+        loop = do
+            tank <- atomically $ do
                 key <- readTChan keyChan
-                let (sprite, tankLocation) = case key of
-                        KeyUp -> (tankSpriteUp, (x, y + 1))
-                let sprite' = translateSprite tankLocation sprite
-                picture <- readTVar pictureVar
-                writeTVar pictureVar (picture // sprite')
-                return tankLocation
-            loop tankLocation
+                ((x, y), _) <- readTVar tankVar
+                let tank = case key of
+                        KeyUp -> ((x, y + 1), North)
+                        KeyDown -> ((x, y - 1), South)
+                        KeyLeft -> ((x - 1, y), West)
+                        KeyRight -> ((x + 1, y), East)
+                writeTVar tankVar tank
+            loop
 
 translateSprite (x, y) sprite = 
     map (first (\(x', y') -> (x + x', y + y'))) sprite
         
-tankSpriteUp = 
-    let tank = [
-            ((0,0),'¤'), ((1,0),'|'), ((2,0),'|'), ((3,0),'¤'), 
-            ((0,1),'¤'), ((1,1),'('), ((2,1),')'), ((3,1),'¤'), 
-            ((0,2),'¤'), ((1,2),'-'), ((2,2),'-'), ((3,2),'¤')] in
-    map (second (\c -> (c, Red))) tank
+        
+tankAsciiNorth = [
+    " || ",
+    "¤||¤",
+    "¤()¤",
+    "¤--¤"]
+
+tankAsciiWest = [    [
+    "¤¤¤ ",
+    "|o==",
+    "¤¤¤ "]
+            
+tankSpriteNorth = toSprite tankAsciiNorth
+
+tankSpriteSouth = toSprite (reverse tankAsciiNorth)
+
+tankSpriteEast = toSprite (map reverse tankAsciiWest)
+
+tankSpriteWast = toSprite tankAsciiWest
+
+toSprite :: [String] -> [((Int, Int), (Char, Color))]
+toSprite lines = toSpriteLines 0 lines
+    where
+        toSpriteLines :: Int -> [String] -> [((Int, Int), (Char, Color))]
+        toSpriteLines row [] = []
+        toSpriteLines row (line : lines) = toSpriteLine 0 row line ++ toSpriteLines (row + 1) lines
+
+        toSpriteLine :: Int -> Int -> String -> [((Int, Int), (Char, Color))]
+        toSpriteLine column row [] = []
+        toSpriteLine column row (char : line) = ((column, row), (char, Red)) : toSpriteLine (column + 1) row line
+            
+
 
 first :: (a -> b) -> (a, c) -> (b, c)
 first f (a, c) = (f a, c)
